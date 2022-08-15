92f9d6bdd5bbde2032e3bfa097236c5db60b085d
AdmiralCringe <109966981+AdmiralCringe@users.noreply.github.com>
2022-08-15 17:45:05 +0300

    Доделал немного магаз

--- Assets/MainMenu/Scripts/OpenScene.cs
+++ Assets/MainMenu/Scripts/OpenScene.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -10,24 +11,38 @@ public class OpenScene : MonoBehaviour
     public GameObject Shop;
     private bool CheckShop = false;
     public GameObject Menu;
-    private bool CheckMenu = true;
-    
+    private bool CheckWheelchair;
+    private bool CheckBed;
+    public Button WheelchairSprite;
+    public Button Bed;
+
     public void ChangeScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
     }
-
+    
     private void Awake()
     {
         Menu.SetActive(true);
+        Shop.SetActive(false);
     }
-
+    
     public void OpenShop()
     {
         if (CheckShop == false)
         {
             Shop.SetActive(true);
+            CheckShop = true;
             Menu.SetActive(false);
+        }
+    }
+    public void CloseShop()
+    {
+        if (CheckShop == true)
+        {
+            Shop.SetActive(false);
+            CheckShop = false;
+            Menu.SetActive(true);
         }
     }
 }
